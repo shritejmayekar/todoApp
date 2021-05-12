b2f2b05fc4243b7aa480edeaf481de9badf6dd4e
Shritej Mayekar <shritejmayekar@gmail.com>
2021-05-13 01:30:43 +0530

    ADDED:readme and development variable changes

---
+++ README.md
@@ -0,0 +1,8 @@
+todoFundoNotes
+
+Contains
+
+Login and Register using PassPortJs
+
+CRUD Operation of the Note
+
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,16 +1,16 @@
 
 module.exports = {
 
-  emailKey:'shritejmayekar69@gmail.com',
-  passwordKey:'fuck8793895204',
+  emailKey:'xxxxxxxxx',
+  passwordKey:'xxxxxxxxx',
   facebook: {
-    clientId:'139717613365382',
-    clientSecret:'ffb6e650208af3f1c90d31ffa39e12e4',
+    clientId:'xxxxxxxxx',
+    clientSecret:'xxxxxxxxx',
     callbackURL:'http://localhost:3000/auth/facebook/callback'
   },
   google: {
-    clientId :'920690214427-ruo8tp6479ksl0f7sqb53ro6fg11e477.apps.googleusercontent.com',
-    clientSecret:'XK-NrKR5AtYUKtNf4kO4n0Xr',
+    clientId :'xxxxxxxxx',
+    clientSecret:'xxxxxxxxx',
     callbackURL:'http://localhost:3000/auth/google/callback'
   }
 }
